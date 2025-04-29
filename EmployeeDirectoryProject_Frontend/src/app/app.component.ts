b8a83fce9d234e2b5e3c62874055dc9a6eabe1e7
GodXero <godxero.dev@gmail.com>
2025-04-29 13:26:02 +0530

    Frontend: auto navigate employee componenet when navigate to login component if already authenticated

--- EmployeeDirectoryProject_Frontend/src/app/app.component.ts
+++ EmployeeDirectoryProject_Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'EmployeeDirectoryProject_Frontend';
+
+  constructor (private authService: AuthService, private router: Router) {
+     this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (this.authService.isAuthenticated()) {
+          this.router.navigate(['/employee']);
+        } else {
+          this.router.navigate(['/login']);
+        }
+      }
+    });
+  }
 }
